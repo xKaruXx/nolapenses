2edc796aeea38e6d81403e96c4cac48ab4a3a63c
Carlos Salini <charlysalini@gmail.com>
2026-05-20 21:34:01 +0000

    chore: versionar fuentes de tailwind local

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./portafolio.html", "./privacidad/**/*.html", "./responsabilidad-usuario/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-electric": "#007BCA",
+        "white-pure": "#FFFFFF",
+        "gray-dark": "#1F1F1F",
+        "gray-medium": "#2A2A2A",
+        "red-coral": "#F94C66",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        display: ["Permanent Marker", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
